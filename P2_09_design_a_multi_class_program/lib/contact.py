a4204ebcb089cba68cfa9b7014e795c12799dcf7
xengeo <xen.geo4@gmail.com>
2023-09-26 12:56:58 +0100

    add contact class and task class

---
+++ P2_09_design_a_multi_class_program/lib/contact.py
@@ -0,0 +1,17 @@
+class Contact:
+        # Public Properties:
+        #   name: string representin contact name
+        #   number: string representing contact number
+
+    def __init__(self, name, number):
+        # Parameters:
+        #   name: string representin contact name
+        #   number: string representing contact number
+        # Side-effects:
+        # sets name and number properties
+        self.name = name
+        self.number = number
+
+    def format():
+        # Returns name and number in user friendly format
+        pass
