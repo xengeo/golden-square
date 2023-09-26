400bca8451937c8ea4007116e8eae6d4d43b4ef2
xengeo <xen.geo4@gmail.com>
2023-09-26 11:12:02 +0100

    add Diary class

--- P2_09_design_a_multi_class_program/lib/diary.py
+++ P2_09_design_a_multi_class_program/lib/diary.py
@@ -4,9 +4,8 @@ import math
 class Diary:
     def __init__(self):
         self._diary_entries = []
-        self._task_list = []
 
-    def add_diary_entry(self, entry) -> None:
+    def add_entry(self, entry) -> None:
         """        
         Parameters: entry: an instance of DiaryEntry
         Returns:    Nothing
@@ -15,42 +14,9 @@ class Diary:
         # Add check to make sure entry is an instance of DiaryEntry
         self._diary_entries.append(entry)
 
-    def add_task_entry(self, entry) -> None:
-        """        
-        Parameters: entry: an instance of DiaryEntry
-        Returns:    Nothing
-        Side-effects:   Adds the entry to the entries list
-        """
-        # Add check to make sure entry is an instance of DiaryEntry
-        self._diary_entries.append(entry)
-
-    def all(self) -> list:
+    def list_all_entries(self) -> list:
         """Returns: A list of instances of DiaryEntry"""
         return self._diary_entries
-
-    def count_words(self):
-        # Returns:
-        #   An integer representing the number of words in all diary entries
-        # HINT:
-        #   This method should make use of the `count_words` method on DiaryEntry.
-        total_words = 0
-        for entry in self._diary_entries:
-            total_words += entry.count_words() 
-        return total_words
-        
-
-    def reading_time(self, wpm):
-        # Parameters:
-        #   wpm: an integer representing the number of words the user can read
-        #        per minute
-        # Returns:
-        #   An integer representing an estimate of the reading time in minutes
-        #   if the user were to read all entries in the diary.
-        total_reading_time = 0
-        for entry in self._diary_entries:
-            total_reading_time += entry.reading_time(wpm) 
-
-        return f"{total_reading_time} minutes"
 
     def find_best_entry_for_reading_time(self, wpm, minutes):
         # Parameters:
